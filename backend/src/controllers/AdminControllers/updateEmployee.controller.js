dff071e0311e4c2717185a2538fb7f6486373f88
akhilshettyym <akhilshettym2003@gmail.com>
2026-04-06 20:28:31 +0530

    update files

--- backend/src/controllers/AdminControllers/updateEmployee.controller.js
+++ backend/src/controllers/AdminControllers/updateEmployee.controller.js
@@ -1,83 +1,3 @@
-// import userModel from "../../models/user.model.js";
-
-// export const updateEmployeeController = async (req, res) => {
-    
-//     try {
-//         const { employeeId } = req.params;
-//         const loggedInUser = req.user;
-
-//         const { firstName, lastName, email, dateOfBirth, designation } = req.body;
-
-//         const employee = await userModel.findOne({
-//             _id: employeeId,
-//             organizationId: loggedInUser.organizationId,
-//         });
-
-//         if (!employee) {
-//             return res.status(404).json({
-//                 success: false,
-//                 message: "Employee not found",
-//             });
-//         }
-
-//         if (employee.employmentStatus !== "ACTIVE") {
-//             return res.status(400).json({
-//                 success: false,
-//                 message: "Only active employee details can be updated",
-//             });
-//         }
-
-//         if (firstName === undefined && lastName === undefined && email === undefined && designation === undefined && dateOfBirth === undefined) {
-//             return res.status(400).json({
-//                 success: false,
-//                 message: "No fields provided for update",
-//             });
-//         }
-
-//         if (email && email !== employee.email) {
-//             const existingEmail = await userModel.findOne({ email });
-//             if (existingEmail) {
-//                 return res.status(409).json({
-//                     success: false,
-//                     message: "Email already in use",
-//                 });
-//             }
-//             employee.email = email;
-//         }
-
-//         if (firstName !== undefined) employee.firstName = firstName;
-//         if (lastName !== undefined) employee.lastName = lastName;
-//         if (designation !== undefined) employee.designation = designation;
-
-//         if (dateOfBirth !== undefined) {
-//             const parsedDate = new Date(dateOfBirth);
-//             if (isNaN(parsedDate.getTime())) {
-//                 return res.status(400).json({
-//                     success: false,
-//                     message: "Invalid DOB format",
-//                 });
-//             }
-//             employee.dateOfBirth = parsedDate;
-//         }
-
-//         await employee.save();
-
-//         return res.status(200).json({
-//             success: true,
-//             message: "Employee details updated successfully",
-//             employee,
-//         });
-
-//     } catch (error) {
-//         return res.status(500).json({
-//             success: false,
-//             message: "Error updating employee details",
-//             error: error.message,
-//         });
-//     }
-// };
-
-
 import userModel from "../../models/user.model.js";
 
 export const updateEmployeeController = async (req, res) => {
@@ -87,9 +7,7 @@ export const updateEmployeeController = async (req, res) => {
         const loggedInUser = req.user;
 
         const { firstName, lastName, email, dateOfBirth, designation } = req.body;
-
-        // SUPER_ADMIN can update any employee
-        // ADMIN only within their org
+        
         const query =
             loggedInUser.role === "SUPER_ADMIN"
                 ? { _id: employeeId }
